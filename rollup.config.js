e5597da1ace87f591e7e4fd1c035553b367692e5
Marco Starker <marco.starker@gmx.net>
2020-04-24 21:19:07 +0200

    dep update

--- rollup.config.js
+++ rollup.config.js
@@ -1,109 +1,94 @@
-import resolve from 'rollup-plugin-node-resolve';
-import progress from 'rollup-plugin-progress';
+import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import {terser} from "rollup-plugin-terser";
 import clear from "rollup-plugin-clear";
 
-const progressOptions = {clearLine: false};
-
-const babelOptionsES2015 = {
-    runtimeHelpers:  false,
+const babelOptionsES5 = {
+    runtimeHelpers: false,
     externalHelpers: false,
-    babelrc:         false,
-    presets:         [
+    babelrc: false,
+    presets: [
         [
             "@babel/env",
             {
                 targets: {
-                    ie:      '11',
-                    edge:    '17',
+                    ie: '11',
+                    edge: '17',
                     firefox: '60',
-                    chrome:  '71',
-                    safari:  '11.1',
+                    chrome: '71',
+                    safari: '11.1',
                 },
             }
         ]
     ],
-    plugins:         [
-        ["@babel/plugin-proposal-class-properties", {loose: true}]
-    ]
 };
 
 const babelOptionsES2018 = {
-    runtimeHelpers:  false,
+    runtimeHelpers: false,
     externalHelpers: false,
-    babelrc:         false,
-    plugins:         [
-        ["@babel/plugin-proposal-class-properties", {loose: true}]
-    ]
+    babelrc: false,
 };
-
-const terserOptions = {sourcemap: true};
 
 // https://rollupjs.org/guide/en#big-list-of-options
 export default [
     // ES2015 Minified
     {
-        input:   './src/index.js',
-        output:  {
-            file:      './dist/urlParametrized.min.js',
-            format:    'umd',
-            name:      'UrlParametrized',
-            compact:   true,
+        input: './src/index.js',
+        output: {
+            file: './dist/urlParametrized.min.js',
+            format: 'umd',
+            name: 'UrlParametrized',
+            compact: true,
             sourcemap: true,
         },
         plugins: [
             resolve(),
             clear({targets: ['./dist']}),
-            progress(progressOptions),
-            babel(babelOptionsES2015),
-            terser(terserOptions),
+            babel(babelOptionsES5),
+            terser(),
         ]
     },
     // ES2015 None-Minified
     {
-        input:   './src/index.js',
-        output:  {
-            file:      './dist/urlParametrized.js',
-            format:    'umd',
-            name:      'UrlParametrized',
-            compact:   false,
+        input: './src/index.js',
+        output: {
+            file: './dist/urlParametrized.js',
+            format: 'umd',
+            name: 'UrlParametrized',
+            compact: false,
             sourcemap: true,
         },
         plugins: [
             resolve(),
-            progress(progressOptions),
-            babel(babelOptionsES2015),
+            babel(babelOptionsES5),
         ]
     },
     // ES Modules Minified
     {
-        input:   './src/index.js',
-        output:  {
-            file:      './dist/urlParametrized.esm.min.js',
-            format:    'esm',
-            compact:   true,
+        input: './src/index.js',
+        output: {
+            file: './dist/urlParametrized.esm.min.js',
+            format: 'esm',
+            compact: true,
             sourcemap: true,
         },
         plugins: [
             resolve(),
-            progress(progressOptions),
             babel(babelOptionsES2018),
-            terser(terserOptions),
+            terser(),
         ]
     },
     // ES Modules None-Minified
     {
-        input:   './src/index.js',
-        output:  {
-            file:      './dist/urlParametrized.esm.js',
-            format:    'esm',
-            compact:   false,
+        input: './src/index.js',
+        output: {
+            file: './dist/urlParametrized.esm.js',
+            format: 'esm',
+            compact: false,
             sourcemap: true,
         },
         plugins: [
             resolve(),
-            progress(progressOptions),
             babel(babelOptionsES2018),
         ]
     },
